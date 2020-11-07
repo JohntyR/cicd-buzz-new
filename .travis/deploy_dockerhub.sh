d11ae9389f95ec9273c28e16d550f5fbaab94eba
John Ronis <jronis@gmail.com>
2020-11-07 18:24:30 +1000

    adding echo

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-NEW_TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]'
-NEW_TAG=$TAG | tr '[:upper:]' '[:lower:]'
+NEW_TRAVIS_REPO_SLUG=$(echo "$TRAVIS_REPO_SLUG" | tr '[:upper:]' '[:lower:]')
+NEW_TAG=$(echo "$TAG" | tr '[:upper:]' '[:lower:]')
 docker build -f Dockerfile -t $NEW_TRAVIS_REPO_SLUG:$NEW_TAG .
 docker push $NEW_TRAVIS_REPO_SLUG:$NEW_TAG
